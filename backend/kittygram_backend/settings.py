4d726cea77af058570beecc7fb5f6a209d15c011
Alex <fantomv1@ya.ru>
2024-03-29 09:32:56 +0500

    Исправил settings, добавил worflow в readme.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,31 +3,17 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from django.core.management.utils import get_random_secret_key
+
+from kittygram_backend.utils import get_allowed_host, get_databases, get_debug_bool, get_secret_key
 
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-def get_secret_key():
-    if os.getenv("SETTINGS_SECRET_KEY") is not None:
-        return os.getenv("SETTINGS_SECRET_KEY")
-    return get_random_secret_key()
-
 SECRET_KEY = get_secret_key()
 
-def get_debug_bool():
-    if os.getenv("SETTINGS_DEBUG") is not None:
-        return os.getenv("SETTINGS_DEBUG").lower() == "true"
-    return True
-
 DEBUG = get_debug_bool()
-
-def get_allowed_host():
-    if os.getenv("SETTINGS_ALLOWED_HOSTS") is not None:
-        return os.getenv("SETTINGS_ALLOWED_HOSTS").split()
-    return []
 
 ALLOWED_HOSTS = get_allowed_host()
 
@@ -73,31 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
-
-
-DATABASES_POSTGRESQL = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
-    }
-}
-
-DATABASES_SQLITE = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-def get_databases():
-    if (os.getenv("SETTINGS_DATABASES_SQLITE") is not None
-        and os.getenv("SETTINGS_DATABASES_SQLITE").lower() == "true"):
-        return DATABASES_SQLITE
-    return DATABASES_POSTGRESQL
 
 DATABASES = get_databases()
 
---
+++ backend/kittygram_backend/utils.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
+
+
+load_dotenv()
+
+
+def get_secret_key():
+    if os.getenv("SETTINGS_SECRET_KEY") is not None:
+        return os.getenv("SETTINGS_SECRET_KEY")
+    return get_random_secret_key()
+
+
+def get_debug_bool():
+    if os.getenv("SETTINGS_DEBUG") is not None:
+        return os.getenv("SETTINGS_DEBUG").lower() == "true"
+    return True
+
+
+def get_allowed_host():
+    if os.getenv("SETTINGS_ALLOWED_HOSTS") is not None:
+        return os.getenv("SETTINGS_ALLOWED_HOSTS").split()
+    return []
+
+
+DATABASES_POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
+}
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES_SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+def get_databases():
+    if (os.getenv("SETTINGS_DATABASES_SQLITE") is not None
+       and os.getenv("SETTINGS_DATABASES_SQLITE").lower() == "true"):
+        return DATABASES_SQLITE
+    return DATABASES_POSTGRESQL
